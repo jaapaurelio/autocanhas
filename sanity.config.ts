2b61b0ba32a1b73a76c11edce79833ec6c79c1ce
Aurelio Pita <jaapaurelio@gmail.com>
2022-12-25 22:35:10 +0000

    chore: improve readme

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,13 +3,16 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
   basePath: "/studio",
   name: "Auto_Canhas_Studio",
   title: "auto-canhas",
 
-  projectId: "if9zk5hh",
-  dataset: "production",
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
